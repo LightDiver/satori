9d2d2077bee2b3a4b6b479479353f1eaef237d43
Serj <light.diver.10@gmail.com>
2015-11-26 19:40:04 +0200

    - Реєстрація нового користувача. Валідація форми (Альфа)

--- source/java/src/main/lightdiver/skim/model/RegistrationNewUser.java
+++ source/java/src/main/lightdiver/skim/model/RegistrationNewUser.java
@@ -6,7 +6,11 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
 import javax.faces.context.FacesContext;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Serj on 24.11.2015.
@@ -24,12 +28,13 @@ public class RegistrationNewUser {
     protected String userLang = "UA";
 
     public String registr(){
-        if (sex.equals("Муж")){
+       /* if (sex.equals("Муж")){
             sex = "M";
         }
         else {
             sex = "W";
         }
+        */
         String res = new Users().registr(userName, hashPass, userPIB, userEMail, sex, userLang);
         if (res == null) {
         FacesContext.getCurrentInstance().addMessage(null,
@@ -40,8 +45,38 @@ public class RegistrationNewUser {
         }
         return "#";
     }
+    public void validUserEMail(FacesContext context, UIComponent comp,
+                                Object value) {
+        final String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\." +
+                "[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*" +
+                "(\\.[A-Za-z]{2,})$";
+
+        Pattern pattern;
+        Matcher matcher;
+        pattern = Pattern.compile(EMAIL_PATTERN);
+        String email = (String)value;
+        matcher = pattern.matcher(email);
 
 
+        if(!matcher.matches()){
+            ((UIInput) comp).setValid(false);
+
+            FacesMessage message = new FacesMessage("E-mail validation failed.",
+                    "Invalid E-mail format");
+            context.addMessage(comp.getClientId(context), message);
+        }
+    }
+    public void validUserName(FacesContext context, UIComponent comp,
+                               Object value) {
+        String username = (String)value;
+        if (username.length() < 5) {
+            ((UIInput) comp).setValid(false);
+
+            FacesMessage message = new FacesMessage("User Login validation failed.",
+                    "User Name length must more 5 symbols");
+            context.addMessage(comp.getClientId(context), message);
+        }
+    }
     public String getUserName() {
         return userName;
     }
